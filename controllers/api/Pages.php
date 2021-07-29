2ffe4d7c29b236b81b3246c174621ff6340f8a95
Octavian Popa <octavian.popa@sprintsoft.ro>
2021-07-29 23:15:02 +0200

    Get pages

---
+++ controllers/api/Pages.php
@@ -0,0 +1,64 @@
+<?php namespace SprintSoft\ApiGeneral\Controllers\Api;
+
+use Backend\Classes\Controller;
+use Cms\Classes\Theme;
+use RainLab\Pages\Classes\Page;
+
+class Pages extends Controller
+{
+    public $implement = [    ];
+
+    public function __construct()
+    {
+        // parent::__construct();
+    }
+
+    public function index()
+    {
+        $theme = Theme::getEditTheme();
+        $pages = Page::listInTheme($theme, true);
+        $arPages = [];
+        foreach ($pages as $page) {
+            $arPages[] = [
+                'title' => $page->title,
+                'url' => $page->url,
+                'content' => $page->getProcessedMarkup(),
+                'seo_params' => [
+                    'seo_title' => $page->meta_title,
+                    'seo_description' => $page->meta_description,
+                    'seo_keywords' => $page->seo_keywords,
+                    'robot_index' => $page->robot_index,
+                    'robot_follow' => $page->robot_follow,
+                    'canonical_url' => $page->canonical_url,
+                ]
+            ];
+        }
+       return response()->json($arPages);
+
+    }
+
+
+
+    public function getPage($url)
+    {
+        $theme = Theme::getEditTheme();
+        $page = Page::load($theme, $url);
+        $arPage = [
+            'title' => $page->title,
+            'url' => $page->url,
+            'content' => $page->getProcessedMarkup(),
+            'seo_params' => [
+                'seo_title' => $page->meta_title,
+                'seo_description' => $page->meta_description,
+                'seo_keywords' => $page->seo_keywords,
+                'robot_index' => $page->robot_index,
+                'robot_follow' => $page->robot_follow,
+                'canonical_url' => $page->canonical_url,
+            ]
+        ];
+        return response()->json($arPage);
+
+
+    }
+
+}
---
+++ routes.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::prefix('api/v1')
+    // ->namespace('SprintSoft\ApiGeneral')
+    ->middleware('api')
+    ->name('apigeneral.')
+    ->group(
+        function () {
+            Route::get('pages', 'SprintSoft\ApiGeneral\Controllers\Api\Pages@index')->name('pages');
+            Route::get('pages/{url}', 'SprintSoft\ApiGeneral\Controllers\Api\Pages@getPage')->name('page.get');
+});
+
+
